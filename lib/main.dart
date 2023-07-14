6aca21302fa17bd9c0f7736878a0699a8f638421
Joao Luiz <joaoluizmarcelinoribeiro@gmail.com>
2023-07-14 12:11:41 -0300

    adding INTL and formating files

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'models/transaction.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 main() => runApp(ExpensesApp());
 
@@ -27,7 +28,6 @@ class MyHomePage extends StatelessWidget {
       )
   ];
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -49,7 +49,49 @@ class MyHomePage extends StatelessWidget {
           Column(
             children: _transactions.map((tr) {
               return Card(
-                child: Text(tr.title),
+                child: Row(
+                  children: <Widget>[
+                      Container(
+                        margin: EdgeInsets.symmetric(
+                          horizontal: 15,
+                          vertical: 10
+                        ),
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.purple,
+                            width: 2,
+                          )
+                        ),
+                        padding: EdgeInsets.all(10),
+                        child: Text(
+                         'R\$  ${tr.value.toStringAsFixed(2)}',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                              color: Colors.purple,
+                            ),
+                          ),
+                      ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              tr.title,
+                              style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            Text(
+                              DateFormat('d MMM y').format(tr.date),
+                              style: TextStyle(
+                                color: Colors.grey,
+                              ),
+                            ),
+                          ],
+                        ),
+                    ],
+                  ),
               );
             }).toList(),
           ),
